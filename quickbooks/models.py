c37832d212ee82afeb7aebc86861f18f7533e3fa
jaywhy13 <jeanmark.wright@gmail.com>
2015-05-06 12:35:38 -0500

    Changed txn_id to be the primary key for bill and removed list_id.

--- quickbooks/models.py
+++ quickbooks/models.py
@@ -320,13 +320,12 @@ class QBVendor(models.Model):
 class QBBill(models.Model):
 
     txn_date = models.CharField(max_length=255, blank=True, null=True) # TxnDate
-    txn_id = models.CharField(max_length=255, blank=True, null=True) # TxnID
+    txn_id = models.CharField(max_length=255, primary_key=True) # TxnID
     due_date = models.CharField(max_length=255, blank=True, null=True) # DueDate
     ref_number = models.CharField(max_length=2500, blank=True, null=True) # RefNumber
     memo = models.CharField(max_length=2500, blank=True, null=True) # Memo
     exchange_rate = models.CharField(max_length=2500, blank=True, null=True) # ExchangeRate
 
-    list_id = models.CharField(max_length=2500, primary_key=True)  # ListID
     is_pending = models.CharField(max_length=2500, blank=True, null=True)  # IsPending
     is_paid = models.CharField(max_length=2500, blank=True, null=True) # IsPaid
     edit_sequence = models.CharField(max_length=2500, blank=True, null=True)  # EditSequence
